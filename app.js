fc33600a7fa802b890504d02bf4a6b48348ee673
adrianagraybill <adriana.graybill@gmail.com>
2019-10-21 19:48:58 -0700

    it persistsgit add .

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ angular
   .controller('CounterController', function() {
     this.number = 0;
 
-    this.increment = () => { this.number += 1 }
-    this.decrement = () => { this.number -= 1 }
-    this.reset = () => { this.number = 0 }
+    if (typeof(Storage) !== "undefined") {
+      this.number = JSON.parse(localStorage.getItem('currentValue'));
+
+      this.increment = () => { 
+        this.number += 1; 
+        localStorage.setItem('currentValue', JSON.stringify(this.number));
+      }
+
+      this.decrement = () => { 
+        this.number -= 1; 
+        localStorage.setItem('currentValue', JSON.stringify(this.number));
+      }
+
+      this.reset = () => { 
+        this.number = 0; 
+        localStorage.setItem('currentValue', JSON.stringify(this.number));
+      }
+    }
   });
